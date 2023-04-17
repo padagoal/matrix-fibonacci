69258f4cdeaaf59185274fcc3d45e2902ea9f24b
Pablo Gonzalez <pablo.gonzalez@somosetica.com>
2023-04-17 14:22:33 -0400

    Fix: FrontendHTML

--- src/main/java/com/r2/fibonaccimatrix/FibonacciAPI.java
+++ src/main/java/com/r2/fibonaccimatrix/FibonacciAPI.java
@@ -2,6 +2,7 @@ package com.r2.fibonaccimatrix;
 
 import com.r2.fibonaccimatrix.annotation.ApiLog;
 import com.r2.fibonaccimatrix.service.FibonacciSpiralService;
+import com.r2.fibonaccimatrix.service.fibonacciResponse;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +17,8 @@ public class FibonacciAPI {
     @ApiLog
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/spiral")
-    public int[][] fibonacciMatrix(@RequestParam(value = "row", defaultValue = "5") Integer row,
-                                @RequestParam(value = "col", defaultValue = "5") Integer col){
+    public fibonacciResponse fibonacciMatrix(@RequestParam(value = "row", defaultValue = "5") Integer row,
+                                             @RequestParam(value = "col", defaultValue = "5") Integer col){
 
 
         FibonacciSpiralService fibonacciSpiralService = new FibonacciSpiralService(row, col);
@@ -27,7 +28,8 @@ public class FibonacciAPI {
         for (int[] rowPrint : matrix) {
             System.out.println(Arrays.toString(rowPrint));
         }
-
-        return matrix;
+        fibonacciResponse response = new fibonacciResponse();
+        response.setMatrix(matrix);
+        return response;
     }
 }
---
+++ src/main/java/com/r2/fibonaccimatrix/service/fibonacciResponse.java
@@ -0,0 +1,11 @@
+package com.r2.fibonaccimatrix.service;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class fibonacciResponse {
+
+    int[][] matrix;
+}
